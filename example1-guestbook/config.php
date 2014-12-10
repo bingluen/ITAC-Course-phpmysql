f260d1db985dfc0af175c1d836021e1b203cd874
erickson-makotoki <a820224@gmail.com>
2014-12-10 19:47:15 +0800

    do sql query

--- example1-guestbook/config.php
+++ example1-guestbook/config.php
@@ -2,7 +2,7 @@
 
 define('Host', '127.0.0.1');
 define('DatabaseType', 'mysql');
-define('DatabaseUser', 'itac_demo');
-define('DatabasePassword', 'NR8WMFWbmLHGD8hq');
-define('DatabaseName', 'itac_demo');
+define('DatabaseUser', 'demo');
+define('DatabasePassword', 'MFSXWx74BYUEJD36');
+define('DatabaseName', 'demo');
 ?>
--- example1-guestbook/test.php
+++ example1-guestbook/test.php
@@ -1,7 +1,14 @@
 <?php
-	require("config.php")
+	require("config.php");
 	$dsn = DatabaseType .':host=' . Host . ';dbname=' . DatabaseName;
 	$dbh = new PDO ($dsn, DatabaseUser, DatabasePassword);
 
-	$sql = "SELECT `email` FROM `guestbook` WHERE `id` = '1';";
-?>
+	$sql = "SELECT `email` FROM `gustbook` WHERE `id` = '1';";
+
+	$sth = $dbh->prepare($sql);
+
+	$sth->execute();
+
+	var_dump($sth->fetch());
+	var_dump($sth->fetchAll());
+?>	
