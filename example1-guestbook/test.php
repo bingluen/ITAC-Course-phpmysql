03eadd0a1b34f8d0fe91030b09e80d2b05e8b43f
erickson-makotoki <a820224@gmail.com>
2014-12-10 20:18:01 +0800

    do insert

--- example1-guestbook/test.php
+++ example1-guestbook/test.php
@@ -3,19 +3,25 @@
 	$dsn = DatabaseType .':host=' . Host . ';dbname=' . DatabaseName;
 	$dbh = new PDO ($dsn, DatabaseUser, DatabasePassword);
 
-	$sql = "SELECT * FROM `gustbook` WHERE `id` = ?;";
+	$sql = "SELECT * FROM `gustbook` WHERE `id` = ? AND `name` = ?;";
 
 	$sth = $dbh->prepare($sql);
 
-	$sth->execute(array(1));
+	$sth->execute(array(1, 'test'));
 
 	echo "<pre>";
 	var_dump($sth->fetch());
 	//var_dump($sth->fetchAll());
 	
-	$sth->execute(array(2));
-	var_dump($sth->fetch());
-	$sth->execute(array(3));
-	var_dump($sth->fetch());
+	//$sth->execute(array(2));
+	//var_dump($sth->fetch());
+	//$sth->execute(array(3));
+	//var_dump($sth->fetch());
 	echo "</pre>";
+
+	$sql = "INSERT INTO `gustbook` (`name`, `email`, `messages`) VALUES ( ?, ? ,?);";
+
+	$sth = $dbh->prepare($sql);
+
+	var_dump($sth->execute(array('erickson', 'erickson@gmail.com', 'itac')));
 ?>	
