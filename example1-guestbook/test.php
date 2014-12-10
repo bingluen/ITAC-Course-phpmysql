19259a7a8c283a324031dc54ba1496111c871db6
erickson-makotoki <a820224@gmail.com>
2014-12-10 20:04:45 +0800

    using parm

--- example1-guestbook/test.php
+++ example1-guestbook/test.php
@@ -3,12 +3,19 @@
 	$dsn = DatabaseType .':host=' . Host . ';dbname=' . DatabaseName;
 	$dbh = new PDO ($dsn, DatabaseUser, DatabasePassword);
 
-	$sql = "SELECT `email` FROM `gustbook` WHERE `id` = '1';";
+	$sql = "SELECT * FROM `gustbook` WHERE `id` = ?;";
 
 	$sth = $dbh->prepare($sql);
 
-	$sth->execute();
+	$sth->execute(array(1));
 
+	echo "<pre>";
 	var_dump($sth->fetch());
-	var_dump($sth->fetchAll());
+	//var_dump($sth->fetchAll());
+	
+	$sth->execute(array(2));
+	var_dump($sth->fetch());
+	$sth->execute(array(3));
+	var_dump($sth->fetch());
+	echo "</pre>";
 ?>	
