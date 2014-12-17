5e4fa35004f96feadff7425881e6b464fd609bb7
erickson-makotoki <a820224@gmail.com>
2014-12-17 19:06:33 +0800

    add getMessages.php

---
+++ example1-guestbook/getMessages.php
@@ -0,0 +1,14 @@
+<?php
+	require("config.php");
+	$dsn = DatabaseType .':host=' . Host . ';dbname=' . DatabaseName;
+	$dbh = new PDO ($dsn, DatabaseUser, DatabasePassword);
+
+	$sql = "SELECT * FROM `guestbook`;";
+	$sth = $dbh->prepare($sql);
+	$sth->execute();
+	$result = $sth->fetchAll();
+	
+	echo '<pre>';
+	var_dump($result);
+	echo '</pre>';
+?>
